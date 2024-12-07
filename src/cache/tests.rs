ebdfa900f7fc6bb6c05ac42ba9535d4f8541e4bc
Guruprasath Balasubramanian <107531265+guruprasath-sterna@users.noreply.github.com>
2024-12-07 11:16:10 +0530

    fixed bugs

--- src/cache/tests.rs
+++ src/cache/tests.rs
@@ -1,15 +1,19 @@
 #[cfg(test)]
 mod queue_tests {
     use core::ops::Range;
+
     use crate::{
         cache::{CacheImpl, NoCache, PagePointerCache, PageStateCache},
         mock_flash::{self, FlashStatsResult, WriteCountCheck},
         queue::{peek, pop, push},
         AlignedBuf,
     };
+
     use futures_test::test;
+
     const NUM_PAGES: usize = 4;
     const LOOP_COUNT: usize = 2000;
+
     #[test]
     async fn no_cache() {
         assert_eq!(
@@ -24,16 +28,6 @@ mod queue_tests {
         );
     }
 
-    
-        
-          
-    
-
-        
-        Expand All
-    
-    @@ -37,7 +37,7 @@ mod queue_tests {
-  
     #[test]
     async fn page_state_cache() {
         assert_eq!(
@@ -48,16 +42,6 @@ mod queue_tests {
         );
     }
 
-    
-        
-          
-    
-
-        
-        Expand All
-    
-    @@ -51,7 +51,7 @@ mod queue_tests {
-  
     #[test]
     async fn page_pointer_cache() {
         assert_eq!(
@@ -72,25 +56,18 @@ mod queue_tests {
         );
     }
 
-    
-          
-            
-    
-
-          
-          Expand Down
-    
-    
-  
     async fn run_test(cache: &mut impl CacheImpl) -> FlashStatsResult {
         let mut flash =
             mock_flash::MockFlashBase::<NUM_PAGES, 1, 256>::new(WriteCountCheck::Twice, None, true);
         const FLASH_RANGE: Range<u32> = 0x00..0x400;
         let mut data_buffer = AlignedBuf([0; 1024]);
+
         let start_snapshot = flash.stats_snapshot();
+
         for i in 0..LOOP_COUNT {
             println!("{i}");
             let data = vec![i as u8; i % 20 + 1];
+
             println!("PUSH");
             push(&mut flash, FLASH_RANGE, cache, &data, true)
                 .await
@@ -122,20 +99,26 @@ mod queue_tests {
             );
             println!("DONE");
         }
+
         start_snapshot.compare_to(flash.stats_snapshot())
     }
 }
+
 #[cfg(test)]
 mod map_tests {
     use core::ops::Range;
+
     use crate::{
         cache::{KeyCacheImpl, KeyPointerCache, NoCache, PagePointerCache, PageStateCache},
         map::{fetch_item, store_item},
         mock_flash::{self, FlashStatsResult, WriteCountCheck},
         AlignedBuf,
     };
+
     use futures_test::test;
+
     const NUM_PAGES: usize = 4;
+
     #[test]
     async fn no_cache() {
         assert_eq!(
@@ -149,6 +132,7 @@ mod map_tests {
             }
         );
     }
+
     #[test]
     async fn page_state_cache() {
         assert_eq!(
@@ -162,6 +146,7 @@ mod map_tests {
             }
         );
     }
+
     #[test]
     async fn page_pointer_cache() {
         assert_eq!(
@@ -175,6 +160,7 @@ mod map_tests {
             }
         );
     }
+
     #[test]
     async fn key_pointer_cache_half() {
         assert_eq!(
@@ -188,6 +174,7 @@ mod map_tests {
             }
         );
     }
+
     #[test]
     async fn key_pointer_cache_full() {
         assert_eq!(
@@ -201,20 +188,25 @@ mod map_tests {
             }
         );
     }
+
     async fn run_test(cache: &mut impl KeyCacheImpl<u16>) -> FlashStatsResult {
         let mut flash =
             mock_flash::MockFlashBase::<NUM_PAGES, 1, 256>::new(WriteCountCheck::Twice, None, true);
         const FLASH_RANGE: Range<u32> = 0x00..0x400;
         let mut data_buffer = AlignedBuf([0; 128]);
+
         const LENGHT_PER_KEY: [usize; 24] = [
             11, 13, 6, 13, 13, 10, 2, 3, 5, 36, 1, 65, 4, 6, 1, 15, 10, 7, 3, 15, 9, 3, 4, 5,
         ];
+
         let start_snapshot = flash.stats_snapshot();
+
         for _ in 0..100 {
             const WRITE_ORDER: [usize; 24] = [
                 15, 0, 4, 22, 18, 11, 19, 8, 14, 23, 5, 1, 16, 10, 6, 12, 20, 17, 3, 9, 7, 13, 21,
                 2,
             ];
+
             for i in WRITE_ORDER {
                 store_item(
                     &mut flash,
@@ -227,10 +219,12 @@ mod map_tests {
                 .await
                 .unwrap();
             }
+
             const READ_ORDER: [usize; 24] = [
                 8, 22, 21, 11, 16, 23, 13, 15, 19, 7, 6, 2, 12, 1, 17, 4, 20, 14, 10, 5, 9, 3, 18,
                 0,
             ];
+
             for i in READ_ORDER {
                 let item = fetch_item::<u16, &[u8], _>(
                     &mut flash,
@@ -242,10 +236,13 @@ mod map_tests {
                 .await
                 .unwrap()
                 .unwrap();
+
                 // println!("Fetched {item:?}");
+
                 assert_eq!(item, vec![i as u8; LENGHT_PER_KEY[i]]);
             }
         }
+
         start_snapshot.compare_to(flash.stats_snapshot())
     }
 }
